1feb04d71a59352e1278871724ad34f1fb63af04
geuluk <geuluk@gmail.com>
2020-12-01 15:49:07 +0100

    add login POST route

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('bidy-parser');
+const bodyParser = require('body-parser');
 
 app.use(bodyParser.json())
 
 const mockUserData=[
-    {name:'Mark'},
-    {name:'Jill'}
+	{name:'Mark'},
+	{name:'Jill'}
 ]
 
-app.get('/users', function(req,res){
-    res.json({
-        success: true,
-        message: 'successfully got user. Nice!',
-        users: mockUserData
-    })
+app.get('/users',function(req,res){
+	res.json({
+		success: true,
+		message: 'successfully got users. Nice!',
+		users: mockUserData
+	})
 })
-
 // colons are used as variables that be viewed in the params
 app.get('/users/:id',function(req,res){
 	console.log(req.params.id)
@@ -28,30 +27,28 @@ app.get('/users/:id',function(req,res){
 })
 
 app.post('/login',function(req,res){
-    // Typically passwords are encrypted using something like bcrypt before sending to database
-    const username=req.body.username;
-    const password=req.body.password;
+	// Typically passwords are encrypted using something like bcrypt before sending to database
+	const username=req.body.username;
+	const password=req.body.password;
 
-    	// This should come from the database
+	// This should come from the database
 	const mockUsername="billyTheKid";
-    const mockPassword="superSecret";
-    
-    if (username===mockUsername && password===mockPassword){
-        // In practice, use JSON web token sign method here to make an encrypted token
-        res.json({
-            success: true,
-            message: 'password and username match!',
-            token: 'encrypted token goes here'
-        })
-    } else {
-            res.json({
-                success: false,
-                message: 'password and usarname do not march'
-            })
-    }
+	const mockPassword="superSecret";
+
+	if (username===mockUsername && password===mockPassword){
+		// In practice, use JSON web token sign method here to make an encrypted token
+		res.json({
+			success: true,
+			message: 'password and username match!',
+			token: 'encrypted token goes here'
+		})
+	} else {
+		res.json({
+			success: false,
+			message: 'password and username do not match'
+		})
+	}
 
 })
 
-app.listen(8000,function(){
-    console.log("server is listening")
-})
+app.listen(8000,function(){console.log('server is listening')})
